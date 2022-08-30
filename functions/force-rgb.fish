19ca64155deb8a74be3800442200ea247f461a7a
Dang <huynhminhdang@gmail.com>
2022-08-30 07:02:26 +0700

    simplify jq script

--- functions/force-rgb.fish
+++ functions/force-rgb.fish
@@ -8,19 +8,18 @@ function force-rgb --description "Force RGB mode for monitor that defaults to YP
     end
   end
   for plist in /Library/Preferences/com.apple.windowserver.displays.plist ~/Library/Preferences/ByHost/com.apple.windowserver.displays.*.plist
-    set --local f1 (mktemp)
-    set --local f2 (mktemp)
-    set --local f3 (mktemp)
-    cp $plist $f1
-    plutil -convert json $f1
-    jq --argjson LinkDescription '{"Range":1,"BitDepth":8,"EOTF":0,"PixelEncoding":0}' '(. | select(.DisplaySets != null) | .DisplaySets.Configs | .[] | if type=="array" then . else (select(.DisplayConfig != null) | .DisplayConfig) end | .[] | .LinkDescription) |= $LinkDescription' $f1 > $f2 2>/dev/null
-    jq --argjson LinkDescription '{"Range":1,"BitDepth":8,"EOTF":0,"PixelEncoding":0}' '(. | select(.DisplayAnyUserSets != null) | .DisplayAnyUserSets.Configs | .[] | if type=="array" then . else (select(.DisplayConfig != null) | .DisplayConfig) end | .[] | .LinkDescription) |= $LinkDescription' $f2 > $f3 2>/dev/null
-    echo '{}' | jq --argfile f1 $f1 --argfile f3 $f3 'if $f1 == $f3 then empty else null|halt_error(99) end'
+    set --local original (mktemp)
+    set --local patched (mktemp)
+    cp $plist $original
+    plutil -convert json $original
+    jq --argjson LinkDescription '{"Range":1,"BitDepth":8,"EOTF":0,"PixelEncoding":0}' '( .. | select(.CurrentInfo?) | .LinkDescription ) |= $LinkDescription' $original > $patched 2>/dev/null
+    echo 'null' | jq --argfile original $original --argfile patched $patched 'if $original != $patched then halt_error(99) else empty end'
     if not test $status -eq 0
 
       set_color yellow
       if set --query --function dryrun
         echo $plist need to be fixed
+        diff --side-by-side (jq '.' $original | psub) $patched
       else
         echo Fixing $plist
       end
@@ -29,16 +28,16 @@ function force-rgb --description "Force RGB mode for monitor that defaults to YP
       set fixes (math $fixes + 1)
 
       if not set --query --function dryrun
-        plutil -convert binary1 $f3
+        plutil -convert binary1 $patched
         if string match --entire --quiet "$HOME/*" $plist
           cp $plist $plist.bak
           chflags nouchg $plist
-          cp $f3 $plist
+          cp $patched $plist
           chflags uchg $plist
         else
           sudo cp $plist $plist.bak
           sudo chflags nouchg $plist
-          sudo cp $f3 $plist
+          sudo cp $patched $plist
           sudo chflags uchg $plist
         end
       end
